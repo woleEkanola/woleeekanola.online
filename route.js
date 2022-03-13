5e65ee1d4d2d7fe360410998d1ecb76b0d5baefe
woleekanola <woleekanola@gmail>
2022-03-13 22:24:27 +0100

    basic router setup

--- route.js
+++ route.js
@@ -1,24 +1,44 @@
 // on page load confirm the uri and adjust view accordingly 
+
 // listen to nav click
 // change the uri value
 // switch the view accordingly
+var slider = document.getElementById('sld')
 
 const router = {
-    locations: ['about-me', 'tech-stack', 'my-projects', 'courses', 'contact-me'],
+    locations: ['about-me', 'tech-stack', 'projects', 'courses', 'contact-me'],
     current: '',
     target: '',
     history: []
 
 }
+switchRoute(setCurrRoute())
 
 function setCurrRoute() {
-    var x = window.location.hash
+    var y = window.location.hash.split('#')
+    var x = y[1]
     router.history.push(router.current == '' ? 'about-me' : router.current)
     router.current = x.length == 0 ? 'about-me' : x
+    return x
 }
 
-function switchRoute() {
-    switch
+
+function switchRoute(x) {
+
+    if (router.locations.indexOf(x) >= 0) {
+        if (router.locations.indexOf(x) == 0) {
+            slider.style.marginLeft = '0px'
+        } else {
+            console.log(slider)
+
+            slider.style.marginLeft = ` ${router.locations.indexOf(x) * -1000}px`
+        }
+        //  `${? 0: () + (router.locations.indexOf(x) * 100)}px`
+    }
 
 
 }
+
+window.addEventListener('popstate', function(e) {
+    switchRoute(setCurrRoute())
+})
