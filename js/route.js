7d088d38f5e015a424f2ef6bb61bfb37a1c19832
woleekanola <woleekanola@gmail>
2022-03-16 01:23:58 +0100

    restructuring the router

--- js/route.js
+++ js/route.js
@@ -12,33 +12,66 @@ const router = {
     history: []
 
 }
-switchRoute(setCurrRoute())
+
 
 function setCurrRoute() {
     var y = window.location.hash.split('#')
     var x = y[1]
     router.history.push(router.current == '' ? 'about-me' : router.current)
-    router.current = x ? 'about-me' : x
-    return x
+    router.current == x ? 'about-me' : x
+    console.log(x)
+    switchRoute(x)
 }
 
 
 function switchRoute(x) {
 
-    if (router.locations.indexOf(x) >= 0) {
-        if (router.locations.indexOf(x) == 0) {
-            slider.style.marginLeft = '0px'
-        } else {
-            console.log(slider)
+    // if (router.locations.indexOf(x) >= 0) {
 
-            slider.style.marginLeft = ` ${router.locations.indexOf(x) * -1000}px`
-        }
-        //  `${? 0: () + (router.locations.indexOf(x) * 100)}px`
+    //     router.locations.forEach(lc => {
+    //         if (lc == x) {
+    //             console.log(x)
+    //             let y = document.getElementById(x)
+    //             console.log(y)
+    //             y.style.display = 'block'
+    //                 // y.style.left = '0 !important'
+    //         } else {
+    //             let y = document.getElementById(x)
+    //             y.style.display = 'none'
+    //                 // y.style.left = '600%'
+    //         }
+    //     })
+
+    // }
+    //  `${? 0: () + (router.locations.indexOf(x) * 100)}px`
+
+    switch (x) {
+
+        case router.locations[0]:
+            slider.style.marginLeft = '0%'
+            break
+
+        case router.locations[1]:
+            slider.style.marginLeft = '-100%'
+            break
+
+        case router.locations[2]:
+            slider.style.marginLeft = '-200%'
+            break
+        case router.locations[3]:
+            slider.style.marginLeft = '-300%'
+            break
+        case router.locations[4]:
+            slider.style.marginLeft = '-400%'
+            break
+
+
     }
-
-
 }
 
+
+
+
 window.addEventListener('popstate', function(e) {
-    switchRoute(setCurrRoute())
+    setCurrRoute()
 })
